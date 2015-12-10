9097203b91593b9842b3aad2d9ff9b6daddbf1e9
Jonatan Pedersen <jp@jonatanpedersen.com>
2015-12-10 22:07:27 +0100

    ui

--- projects.js
+++ projects.js
@@ -20,7 +20,7 @@ export function makeCreateProject (db) {
       projects.insert({
         name: projectName,
         repositoryUrl: repositoryUrl,
-        setttings: {
+        settings: {
           languages: ['en']
         }
       }, (err, res) => {
