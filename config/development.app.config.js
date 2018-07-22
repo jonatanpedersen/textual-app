d2ed139d36ede843eb2cfe0a1ef575ce1603e500
Jonatan Pedersen <jp@jonatanpedersen.com>
2018-07-22 15:42:52 +0200

    open sources

--- config/development.app.config.js
+++ config/development.app.config.js
@@ -1,21 +1,2 @@
 export default {
-  data: './data',
-  host: 'http://localhost',
-  port: 80,
-  session: {
-    secret: 'HYXe2rKZPw4Ar893mrL3W0o8fCIq0X3aE1k868uok1GXUF2gdoEg092YC3Iuqu3b',
-    resave: false,
-    saveUninitialized: true
-  },
-  github: {
-    client_id: 'cf1635091c35b7fe1fb8',
-    client_secret: 'f4bfb04353edd24e69fcda04aeae8e93def8f556',
-    callback_url: 'http://localhost/auth/github/callback'
-  },
-  jwt: {
-    secret: 'K4xEToZq6e9ED7SsxR0D1DSVFXGqRQGj'
-  },
-  mongodb: {
-    connectionString: 'mongodb://localhost:27017/textual-app-test'
-  }
 }
--- config/global.app.config.js
+++ config/global.app.config.js
@@ -1,31 +1,7 @@
 export default {
 	github: {
 		authorizedUsers: [
-			'bifrost',
-			'bobhammer',
-			'bubenshchykov',
-			'eagleeye',
-			'ebdrup',
-			'jonatanpedersen',
-			'kollner',
-			'mamant',
-			'mpushkin',
-			'Oligrand',
-			'sscdebitoor',
-			'wtfil',
-			'philipshurpik',
-			'ajleoni',
-			'hilleer',
-			'alexeydebitoor',
-			'AndreaDebitoor',
-			'Laco21',
-			'lenedebitoor',
-			'PilarPMC',
-			'SergioLozano',
-			'omichelsen',
-			'HannahHLN',
-			'CharlesPlasson',
-			'WhitneyWHI'
+			'jonatanpedersen'
 		]
 	}
 }
